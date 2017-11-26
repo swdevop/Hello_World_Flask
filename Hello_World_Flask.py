c5fa25f1dc397717c0fe4673a7e14902bdf83eba
swdevop <devop@shieldwolfdesigns.com>
2017-11-26 16:02:54 -0600

    further stuff

--- Hello_World_Flask.py
+++ Hello_World_Flask.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    # return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
